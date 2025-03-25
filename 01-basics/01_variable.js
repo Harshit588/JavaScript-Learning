b24dbaeafbe16481d251da2dbbc43449ecb15185
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-03-25 08:45:49 +0000

    let,const,var learning

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 10240611
+let accountEmail = "harshitarya0611@gmail.com"
+var accountMobNo = 9340886343
+accountState = "Madhya Pradesh"
+let accountDivision;
+
+// accountId = 12345678 // can't reassign becouse const(Constant)
+accountMobNo = 123456789
+
+/*
+    var prefer to not use because of block scope , functional scope 
+ */
+
+console.table([accountId, accountEmail, accountMobNo, accountState, accountDivision])
