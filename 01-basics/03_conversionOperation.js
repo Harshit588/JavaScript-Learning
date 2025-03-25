f7b1194529d24db42476f7be5618a67a94bc258d
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-03-25 11:35:07 +0000

    Conversion of DataType in JS

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = false;
+// console.log(typeof score);
+
+let valueInNumber = Number(score); // Convert String to Number
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true  -> 1
+// false -> 0
+// "" -> false
+// "harshit" -> true
+
+const isLoggedIn = 12
+let isLoggedInChanged = Number(isLoggedIn)
+console.log(isLoggedInChanged);
+
+let someNumber = 33
+let itsString = String(someNumber)
+console.log(typeof itsString);
+
+
+
