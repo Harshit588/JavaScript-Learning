1b32e6c84660f853b20e236d1ac23d36f6e84199
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-03-25 12:10:00 +0000

    operation on Datatype

---
+++ 01-basics/04_operationOnDataType.js
@@ -0,0 +1,26 @@
+let value = 7;
+let nagativeValue = -value;
+// console.log(nagativeValue);
+
+// console.log(2 + 2); //4
+// console.log(2 - 2); //0
+// console.log(2 * 2); // 4
+// console.log(2 ** 3); // 8
+// console.log(2 % 3); // 2
+// console.log(3 / 2); // 1.5
+
+// let str1 = "Harshit";
+// let str2 = " Soni";
+// let str3 = str1 + str2;
+// let str4 = str1.concat(str2);
+// console.log(str3);
+// console.log(str4);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log(1 + "2" + 1); //121
+// console.log("1" + 2 + 1); //121
+// console.log(1 + 2 + "2"); //32
+
+console.log(-true);
+
