8485ed904c98bd8c8813418a764b4307e40eebf2
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-03-27 11:42:33 +0000

    while, do-while

---
+++ 05_Loops/02_while.js
@@ -0,0 +1,29 @@
+// while
+
+// while (condition) {}
+
+// print even no 0 t0 20
+// let i = 0;
+// while (i <= 20) {
+//   console.log(`Value of i is :: ${i}`);
+//   i += 2;
+// }
+
+// print array
+// const myArr = ["akshy", "vicky", "salman"];
+// let i = 0;
+// while (i < myArr.length) {
+//   console.log(myArr[i]);
+//   i++;
+// }
+
+//++++++++++++++++++++++++++++++++++++ do-while +++++++++++++++++++++++++++++++++++++++
+// do {
+// } while (condition);
+
+// let x = 1; // op -> 1,2,3,4,5,6,7,8,9,10
+let x = 11; // op -> 11
+do {
+  console.log(`Value of x is : ${x}`);
+  x++;
+} while (x <= 10);
